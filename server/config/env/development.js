c7e94522bef5e70e29e7519f66943009b7c70ff9
abdullahaslam306 <f178135@nu.edu.pk>
2022-04-15 15:42:37 +0500

    addsas

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,11 +1,12 @@
 "use strict";
 module.exports = {
   PORT: 3000,
-  MONGODB_URI: "mongodb://localhost:27017/LegendaryVault",
+  MONGODB_URI:
+    "mongodb://ec2-54-176-199-98.us-west-1.compute.amazonaws.com:27017/LegendaryVault",
   secret: "secret",
   host: "localhost:5000",
   smtpAuth: {
-    user: 'iqrar190@gmail.com',
-    pass: 'Dummy123'
+    user: "iqrar190@gmail.com",
+    pass: "Dummy123"
   }
 };
