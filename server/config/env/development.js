677258fbb78b18cfdf2d38e0f71a1e60a43bf5d0
abdullahaslam306 <f178135@nu.edu.pk>
2022-04-23 01:37:18 +0500

    db changed

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -2,7 +2,7 @@
 module.exports = {
   PORT: 3000,
   MONGODB_URI:
-    "mongodb://ec2-54-176-199-98.us-west-1.compute.amazonaws.com:27017/LegendaryVault",
+    "mongodb://ec2-54-153-114-186.us-west-1.compute.amazonaws.com:27017/LegendaryVault",
   secret: "secret",
   host: "localhost:5000",
   smtpAuth: {
